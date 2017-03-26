aeb534ec03809dbd4e21545e3ce11f6e099e2ad1
pedro almeida <pedro.marc2003@gmail.com>
2017-03-26 11:26:53 -0300

    SINATRA 1.0

--- app.rb
+++ app.rb
@@ -7,6 +7,23 @@ configure :development do
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
+#get '/' do
+  #erb :index
+#end
+
+
+get '/about' do
+  erb :about
+end
+
+get '/team/:username' do
+  #binding.pry
+  puts params[:username]
+  "The username is #{params[:username]}"
+end
+
+
 get '/' do
-  'Hello world!'
+  @usernames = [ 'ssaunier', 'Papillard' ]
+  erb :index
 end
